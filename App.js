f6579ed3c6f754442a2c176be4f5deb9d8b4260b
amitk0551 <uic.18mca1015@gmail.com>
2025-04-18 21:36:08 +0530

    episode 2

--- App.js
+++ App.js
@@ -1,51 +1,53 @@
+import React from "react";
+import ReactDOM from 'react-dom/client'
+
 const heading = React.createElement(
-    "h1",
-    { className: "heading" },
-    "Hello World"
-  );
-  
-  //if you want to create nested elements through React.createElement
-  const parrent = React.createElement(
-    "div",
-  
-    { id: "parent" },
+  "h1",
+  { className: "heading" },
+  "Hello World"
+);
+
+//if you want to create nested elements through React.createElement
+const parrent = React.createElement(
+  "div",
+  { id: "parent", key:"parent" },
+  [
+    React.createElement(
+      "div",
+      { className: "child", key:"child" },
+      "Hello World inside child"
+    ),
     [
       React.createElement(
-        "div",
-        { className: "child" },
-        "Hello World inside child"
+        "h1",
+        { className: "heading", key:"heading" },
+        "Hello World inside h1"
       ),
-      [
-        React.createElement(
-          "h1",
-          { className: "heading" },
-          "Hello World inside h1"
-        ),
-        React.createElement(
-          "h1",
-          { className: "heading" },
-          "Hello World inside h2"
-        ),
-      ],
       React.createElement(
-        "div",
-        { className: "child" },
-        "Hello World inside child"
+        "h1",
+        { className: "heading", key:"heading2" },
+        "Hello World inside h2"
       ),
-      [
-        React.createElement(
-          "h1",
-          { className: "heading" },
-          "Hello World inside h1"
-        ),
-        React.createElement(
-          "h1",
-          { className: "heading" },
-          "Hello World inside h2"
-        ),
-      ],
-    ] //you can also pass array if you want to add multiple elements
-  );
-  
-  const root = ReactDOM.createRoot(document.getElementById("root"));
-  root.render(parrent);
+    ],
+    React.createElement(
+      "div",
+      { className: "child", key:"child2" },
+      "Hello World inside child"
+    ),
+    [
+      React.createElement(
+        "h1",
+        { className: "heading" , key:"heading4"},
+        "Hello World inside h1"
+      ),
+      React.createElement(
+        "h1",
+        { className: "heading", key:"heading6" },
+        "Hello World inside h2"
+      ),
+    ],
+  ] //you can also pass array if you want to add multiple elements
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parrent);
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Namaste-React
+
+
+# Parcel
+  -Create a Dev Build
+  -Create a local server
+  -HMR Hot Module Replacement
+  -File watching Algorithm - which is written in c++
+  -Caching - for faster builds
+  -Parcel does image optimization
+  -Parcel does minification for your production build
+  -Parcel does comperssing our file.
+  -Parcel does bundling in our app.
+  -Consistency Hashing
+  -Code spliting
+  -Differential Bundling means supporting older version browsers
+  -Diagnostic
+  -Error Handling
+  -Give support to HTTPs also
+  -Tree Shaking means remove unused code from the app when making bundle
+  -Parcel will create different build for dev and production when it make production build it will more optimized our code
